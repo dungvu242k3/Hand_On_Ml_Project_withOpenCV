c79ca985adf1bb2bf98bf819a2b127b5c2d306d5
Hand_On_ML_Project_withOpenCV <dungvu24042k3@gmail.com>
2024-01-02 14:37:07 +0700

    baitap

--- chapter03/bai1.py
+++ chapter03/bai1.py
@@ -1,29 +1,44 @@
 import cv2
 import numpy as np
 
-img1 = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter03/image-cat.jpg")
-img2 = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter03/image-dog.jpg")
-G1 = img1.copy()
-G2 = img2.copy()
-gp1 = [G1]
-gp2 = [G2]
+image1 = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter03/image-dog.jpg")
+image2 = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter03/image-cat.jpg")
+
+G = image1.copy()
+gp_cat = [G]
 for i in range(6):
-    G1 = cv2.pyrDown(G1)
-    G2 = cv2.pyrDown(G2)
-lp1 = [gp1[-1]]
-lp2 = [gp2[-1]]
-for i in range(5,0,-1):
-    lp1.append(cv2.subtract(gp1[i-1], cv2.pyrUp(gp1[i])))
-    lp2.append(cv2.subtract(gp2[i-1], cv2.pyrUp(gp2[i])))
+    G = cv2.pyrDown(G)
+    gp_cat.append(G)
+
+G = image2.copy()
+gp_dog = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    gp_dog.append(G)
+
+lp_cat = [gp_cat[5]]
+for i in range(5, 0, -1):
+    GE = cv2.pyrUp(gp_cat[i])
+    L = cv2.subtract(gp_cat[i-1], cv2.resize(GE, gp_cat[i-1].shape[1::-1]))
+    lp_cat.append(L)
+
+lp_dog = [gp_dog[5]]
+for i in range(5, 0, -1):
+    GE = cv2.pyrUp(gp_dog[i])
+    L = cv2.subtract(gp_dog[i-1], cv2.resize(GE, gp_dog[i-1].shape[1::-1]))
+    lp_dog.append(L)
+
 LS = []
-for  l1,l2 in zip(lp1,lp2):
-    rows,cols,dpt = l1.shape
-    ls = np.hstack((l1[:,0:cols//2],l2[:,cols//2:]))
+for l_cat, l_dog in zip(lp_cat, lp_dog):
+    rows, cols, dpt = l_cat.shape
+    ls = np.hstack((l_cat[:, :cols//2], l_dog[:, cols//2:]))
     LS.append(ls)
-img = LS[0]
-for i in range(1,6):
-    img = cv2.pyrUp(img)
-    img = cv2.add(img,LS[i])
-cv2.imshow("KQ",img)
+
+ls_ = LS[0]
+for i in range(1, 6):
+    ls_ = cv2.pyrUp(ls_)
+    ls_ = cv2.add(ls_, LS[i])
+
+cv2.imshow("KQ",ls_)
 cv2.waitKey()
 cv2.destroyAllWindows()
