5945435bbb7dc573e8fec6991c36e64ca3f7aec1
Hand_On_ML_Project_withOpenCV <dungvu24042k3@gmail.com>
2024-01-12 16:58:39 +0700

    baitap

--- chapter03/bai1.py
+++ chapter03/bai1.py
@@ -1,29 +1,17 @@
 import cv2
 import numpy as np
 
-img1 = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter03/image-dog.jpg",1)
-img2 = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter03/image-cat.jpg",1)
+img1 = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter03/image-dog.jpg")
+img2 = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter03/image-cat.jpg")
 
-height1,width1,_ = img1.shape
-height2,width2,_ = img2.shape
+kernel1 = np.linspace(1, 0, img1.shape[1]).reshape(1, -1).repeat(img1.shape[0], axis=0)
+kernel2 = np.linspace(0, 1, img2.shape[1]).reshape(1, -1).repeat(img2.shape[0], axis=0)
 
-blurr1 = cv2.GaussianBlur(img1[:,200:],(15,15),0)
-blurr2 = cv2.GaussianBlur(img2[:,:150],(45,45),0)
+kernel1 = np.dstack([kernel1] * 3)
+kernel2 = np.dstack([kernel2] * 3)
 
-kq1 = np.copy(img1)
-kq2 = np.copy(img2)
+kq= (kernel1 * img1 + kernel2 * img2).astype(np.uint8)
 
-kq1[:,200:] = blurr1
-kq2[:,:150] = blurr2
-
-
-kq = 1.3*kq1 + 0.15*kq2 + -75
-kq = np.clip(kq,10,255).astype('uint8')
-kq3 = 0.2*kq1 + 0.7*kq2 + -10
-kq3 = np.clip(kq3,0,255).astype('uint8')
-result = cv2.addWeighted(kq,0.5,kq3,0.55,-40)
-#cv2.imshow("kq",kq)
-#cv2.imshow("kq3",kq3)
-cv2.imshow("result",result)
-cv2.waitKey()
+cv2.imshow("kq", kq)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
