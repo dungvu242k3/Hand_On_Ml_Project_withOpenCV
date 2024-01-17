0e664684d82efe2526cd1370487b2ae02d22ffc1
Hand_On_ML_Project_withOpenCV <dungvu24042k3@gmail.com>
2024-01-17 15:56:25 +0700

    baitap

--- chapter03/bai2.py
+++ chapter03/bai2.py
@@ -1,40 +1,40 @@
 import cv2
 import numpy as np
+
 img = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter03/image1.jpg")
-img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-laplacian_kernel = np.array([
-    [0, 1, 0],
-    [1, -4, 1],
-    [0, 1, 0]
-])
+box_kernel = np.ones((1,1))/2
+kernel = np.ones((3,3))/9
 
-prewitt_kernel_x = np.array([
+kernelx = np.array([
     [-1, 0, 1],
     [-1, 0, 1],
     [-1, 0, 1]
 ])
-prewitt_kernel_y = np.array([
+kernely = np.array([
     [-1, -1, -1],
     [0, 0, 0],
     [1, 1, 1]
 ])
 
-kernelx = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
-kernely = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
-kernel = np.ones((3, 3), np.float32) / 16
-sobel = cv2.filter2D(img,-1,laplacian_kernel)
+sobel = cv2.filter2D(img,cv2.CV_64F,kernel)
+box_sobel = cv2.filter2D(img,cv2.CV_64F,box_kernel)
+sobelx = cv2.filter2D(img, cv2.CV_64F, kernelx)
+sobely = cv2.filter2D(img, cv2.CV_64F, kernely)
 
-sobelx = cv2.filter2D(sobel,-1,prewitt_kernel_x)
+main_sobel = np.sqrt(sobelx**2 + sobely**2)
+main_sobel = cv2.convertScaleAbs(main_sobel)
 
-sobely = cv2.filter2D(sobel,-1,prewitt_kernel_y)
+sobel_combied = np.sqrt(box_sobel**2 + sobelx**2)
+sobel_combied = cv2.convertScaleAbs(sobel_combied)
 
+secondary_sobel = np.sqrt(sobel**2 + sobely**2)
+secondary_sobel = cv2.convertScaleAbs(secondary_sobel)
 
-sobel_combined1 = cv2.bitwise_xor(sobel,sobelx)
-sobel_combined2 = cv2.bitwise_or(sobel,sobely)
+cv2.imshow("main_sobel",main_sobel)
+cv2.imshow("secondary_sobel",sobel_combied)
+#cv2.imshow("sobel_combined",secondary_sobel)
 
-
-cv2.imshow("kq1",sobel_combined1)
-cv2.imshow("kq2",sobel_combined2)
-cv2.waitKey()
+cv2.waitKey(0)
 cv2.destroyAllWindows()
