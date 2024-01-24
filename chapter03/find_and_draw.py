4183c4355cbbd7cd8ca08a3ba687e109eaa5e50f
Hand_On_ML_Project_withOpenCV <dungvu24042k3@gmail.com>
2024-01-24 23:50:02 +0700

    baitap

--- chapter03/find_and_draw.py
+++ chapter03/find_and_draw.py
@@ -1,48 +1,26 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter04/geometric_shapes.png")
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-_, thresh = cv2.threshold(gray, 205, 255, cv2.THRESH_BINARY_INV)
-contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+image = cv2.imread("C:/Users/dungv/Projects/Hand_On_Ml_Project_withOpenCV/chapter04/geometric_shapes.png")
 
-for i in range (68) :
-    if 0 <= i <= 6 :
-        cv2.drawContours(img, contours, i, (0, 100, 0), 3)
-    elif i == 29 :
-        cv2.drawContours(img, contours, i, (0, 100, 0), 5) 
-    elif 7 <= i <= 11 or i == 24 :
-        cv2.drawContours(img, contours, i , (255,0,0), 3) 
-    elif i == 30 :
-        cv2.drawContours(img, contours, i , (255,0,0), 5) 
-    elif 12 <= i <= 17 or 25 <= i <= 26 :
-        cv2.drawContours(img, contours, i , (255,0,255), 3) 
-    elif i == 28 :
-        cv2.drawContours(img, contours, i , (255,0,255), 5) 
-    elif 18 <= i <= 23 or i == 27 :
-        cv2.drawContours(img, contours, i, (153,136,119), 3) 
-    elif i == 31 :
-        cv2.drawContours(img, contours, i, (153,136,119), 5) 
-    elif 45 <= i <=51 or i == 62 :
-        cv2.drawContours(img, contours, i, (105,105,105),3) 
-    elif i == 33 :
-        cv2.drawContours(img, contours, i, (105,105,105),5)
-    elif 36 <= i <= 43 :
-        cv2.drawContours(img, contours, i, (0,140,255),3) 
-    elif i == 34 :
-        cv2.drawContours(img, contours, i, (0,140,255),5) 
-    elif i == 44 or 52 <= i <=56 or 63 <= i <= 65:
-        cv2.drawContours(img, contours, i, (144,96,205),3) 
-    elif i == 32 :
-        cv2.drawContours(img, contours, i, (144,96,205),5) 
-    elif 57 <= i <=61 or 66 <=i<=67 :
-        cv2.drawContours(img, contours, i, (255,255,100),3)
-    elif i == 35 :
-        cv2.drawContours(img, contours, i, (255,255,100),5)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+_, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
+contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-#cv2.imshow("edges",thresh) 
-cv2.imshow("kq",img)
-cv2.waitKey()
+def get_contour_color(img, contour):
+    mask = np.zeros(img.shape[:2], dtype=np.uint8)
+    cv2.drawContours(mask, [contour], -1, 255, thickness=3)
+
+    mean_color = cv2.mean(img, mask=mask)
+    
+    return mean_color
+
+for contour in contours:
+    color = get_contour_color(image, contour)
+    color = tuple(map(int, color))
+    cv2.drawContours(image, [contour], -1, color, thickness=3)
+
+cv2.imshow("kq", image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
-        
